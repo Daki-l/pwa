4b3d8b9312bc66e7a23f5574a38f06286b9fcafe
David liu <dorismartin12437@gmail.com>
2019-03-06 20:51:36 +0800

    pwa -1

--- sw.js
+++ sw.js
@@ -1,43 +1,17 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
-var cacheStorageKey = 'minimal-pwa-2'
-var cacheList=[
-    '/',
-    'index.html',
-    'main.css',
-    'one.jpg'
-]
-self.addEventListener('install',e =>{
-    e.waitUntil(
-        console.log(e)
-        // caches.open(cacheStorageKey)
-        // .then(cache => cache.addAll(cacheList))
-        // .then(() => self.skipWaiting())
-    )
-})
-// self.addEventListener('fetch',function(e){
-//     e.respondWith(
-//         caches.match(e.request).then(function(response){
-//             if(response != null){
-//                 return response
-//             }
-//             return fetch(e.request.url)
-//         })
-//     )
-// })
-// self.addEventListener('activate',function(e){
-//     e.waitUntil(
-//         //获取所有cache名称
-//         caches.keys().then(cacheNames => {
-//             return Promise.all(
-//                 // 获取所有不同于当前版本名称cache下的内容
-//                 cacheNames.filter(cacheNames => {
-//                     return cacheNames !== cacheStorageKey
-//                 }).map(cacheNames => {
-//                     return caches.delete(cacheNames)
-//                 })
-//             )
-//         }).then(() => {
-//             return self.clients.claim()
-//         })
-//     )
-// })
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/styles/main.css',
+  '/script/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
